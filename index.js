91806aff0974494af108010525880c133aee18f2
rajibjonchhen <rajib.jonchhen@gmail.com>
2021-11-30 16:37:10 +0000

    button added to fetch data

--- index.js
+++
@@ -1,9 +0,0 @@
-
-
-        // fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=eminem")
-        // .then(response => response.json())
-        // .then(data => {
-        //     console.log(data)
-        // })
- 
-console.log(2+2)
