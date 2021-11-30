e7cac78583c2c69c84fbfef01710621ba9f920d1
rajibjonchhen <rajib.jonchhen@gmail.com>
2021-11-30 14:19:29 +0000

    fetch

---
+++ index.js
@@ -0,0 +1,9 @@
+
+
+        // fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=eminem")
+        // .then(response => response.json())
+        // .then(data => {
+        //     console.log(data)
+        // })
+ 
+console.log(2+2)
